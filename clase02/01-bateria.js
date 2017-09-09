f2ef50b91f5b25459025686b4e2a1e6cfd99445b
davzu <davzu_90@hotmail.com>
2017-09-09 10:41:16 -0500

    clase02 20170909

---
+++ clase02/01-bateria.js
@@ -0,0 +1,25 @@
+window.addEventListener("keydown", function (e) {
+	const teclaCodigo = e.keyCode;
+	const teclaPresionada = document.querySelector(`div[data-tecla="${teclaCodigo}"]`);
+	const audioSeleccionado = document.querySelector(`audio[data-tecla="${teclaCodigo}"]`);
+
+	// undefined es tomado como false
+	if (teclaPresionada && audioSeleccionado) {
+		teclaPresionada.classList.add("seleccionada");
+
+		audioSeleccionado.currentTime = 0;  
+		audioSeleccionado.play();
+
+	}
+
+	//console.log(e);
+});
+
+const teclas = document.querySelectorAll(".tecla");
+
+teclas.forEach( tecla => {
+	tecla.addEventListener("transitionend", function(e) {
+		this.classList.remove("seleccionada");
+	});
+});
+
